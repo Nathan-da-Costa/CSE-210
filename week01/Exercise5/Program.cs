c162199e230c01d89bcee82d5b46bbc8bef84390
Nathan-da-Costa <costa.nathhan.sud@gmail.com>
2026-07-11 19:43:20 -0300

    exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Call the functions in order and save their return values
+        DisplayWelcome();
+        
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        
+        int squaredNumber = SquareNumber(userNumber);
+        
+        DisplayResult(userName, squaredNumber);
+    }
+
+    // Function 1: Displays a welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function 2: Prompts for and returns the user's name
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    // Function 3: Prompts for and returns the user's favorite number
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    // Function 4: Calculates and returns the square of a number
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    // Function 5: Displays the final user result
+    static void DisplayResult(string name, int squared)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squared}");
     }
 }
