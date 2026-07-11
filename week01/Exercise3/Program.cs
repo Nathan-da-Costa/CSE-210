509dacd39cba34eeaa90de83f7cdb186bec4cc1d
Nathan-da-Costa <costa.nathhan.sud@gmail.com>
2026-07-11 19:37:03 -0300

    exercice 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain;
+
+        // Outer loop to restart the game
+        do
+        {
+            // Generate a random magic number between 1 and 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("I have chosen a magic number between 1 and 100.");
+
+            // Inner loop for the current guessing round
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++; // Increment the counter
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            // Ask the user if they want to repeat the game
+            Console.Write("Do you want to play again (yes/no)? ");
+            playAgain = Console.ReadLine().ToLower();
+            Console.WriteLine();
+
+        } while (playAgain == "yes");
     }
 }
